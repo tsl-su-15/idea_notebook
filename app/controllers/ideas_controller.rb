08a9199602c0580dbdb39654e4e848dec8f50edc
Arjun Venkataswamy <avenkat2@gmail.com>
2015-07-14 10:31:15 -0500

    completes functionality for show action

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,6 +1,5 @@
 class IdeasController < ApplicationController
   def show
-    # pull up the appropriate idea object from the database
-    # send it down in an instance variable
+    @idea = Idea.find(params['id'])
   end
 end
